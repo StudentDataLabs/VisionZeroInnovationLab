67a4e99c7b90bb025540e8dcc839050e607852ce
L. Bolton <liamthomasbolton@gmail.com>
2016-04-23 22:39:30 +0100

    Update R Code - Mapping Bicycle Casualties.R

--- resources/code/R Code - Mapping Bicycle Casualties.R
+++ resources/code/R Code - Mapping Bicycle Casualties.R
@@ -5,7 +5,7 @@ library(ggmap)
 library(ggplot2)
 
 # Set the working directory and read in the data
-setwd("M:/VisionZeroInnovationLab-master/data") # If you are using a Mac, then you will need to change this line to: setwd("C:/VisionZeroInnovationLab-master/data")
+setwd("M:/VisionZeroInnovationLab-master/data") # If you are using a Mac, then you will need to change this line to: setwd("~/VisionZeroInnovationLab-master/data")
 road <- read.csv("accidents_2009-2014_duplicates_mapping.csv",header = TRUE,stringsAsFactors = FALSE)
 
 # Create a subset containing bicycle crashes
