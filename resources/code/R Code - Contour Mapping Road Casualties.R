05f4b14ff582323ac4c25b2380193c7529e29616
L. Bolton <liamthomasbolton@gmail.com>
2016-04-23 19:13:41 +0100

    Update R Code - Contour Mapping Road Casualties.R

--- resources/code/R Code - Contour Mapping Road Casualties.R
+++ resources/code/R Code - Contour Mapping Road Casualties.R
@@ -1,7 +1,8 @@
 install.packages("ggmap")
 library(ggmap)
 
-# Read in the data
+# Set the working directory and read in the data
+setwd("M:/VisionZeroInnovationLab-master/data")
 accidents <- read.csv("accidents_2009-2014_duplicates_mapping.csv", header = TRUE, stringsAsFactors = FALSE)
 
 # Set the map centre
